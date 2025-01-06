fc40934060a2b5a7fc9e8522c913a2c14c3f5ee3
RoyDevTEAM <roidercito12345@gmail.com>
2025-01-05 23:52:33 -0400

    about

--- src/app/public/about/about.component.ts
+++ src/app/public/about/about.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  // Array de miembros del equipo
+  teamMembers = [
+    {
+      name: 'Roider Millares',
+      role: 'Desarrollador Backend',
+      image: 'assets/images/roider.jpg'  // Ruta de la imagen del miembro
+    },
+    {
+      name: 'carlos Rivera',
+      role: 'Desarrollador Frontend',
+      image: 'assets/images/jonathan.jpg'  // Ruta de la imagen del miembro
+    },
+    {
+      name: 'Rodrigo Ledezma',
+      role: 'Especialista en Marketing Digital',
+      image: 'assets/images/jaime.jpg'  // Ruta de la imagen del miembro
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
