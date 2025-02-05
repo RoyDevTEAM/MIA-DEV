b026d9ed5c88500e9193113a645da4b6582b7e76
RoyDevTEAM <roidercito12345@gmail.com>
2025-02-05 16:49:38 -0400

    img corregidas

--- src/app/public/about/about.component.ts
+++ src/app/public/about/about.component.ts
@@ -11,72 +11,112 @@ export class AboutComponent implements OnInit {
     {
         name: 'Roider Millares',
         role: 'Desarrollador Fullstack',
-        image: 'assets/img/roy.jpg'
+        image: 'assets/img/roy.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
+
     }
     ,
     {
         name: 'Noelia Cuellar Chung',
         role: 'Desarrollador Fullstack',
-        image: 'assets/img/noelia.jpg'
+        image: 'assets/img/noelia.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Carlos Rivera',
         role: 'Marketing y Publicidad Digital',
-        image: 'assets/img/carlos.jpg'
+        image: 'assets/img/carlos.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Rodrigo Ledezma',
         role: 'Desarrollador Movil',
-        image: 'assets/img/rodrigo.jpg'
+        image: 'assets/img/rodrigo.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Camila Gutiérrez',
         role: 'Diseñadora Gráfica',
-        image: 'assets/img/camila.jpg'
+        image: 'assets/img/camila.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Moisés Moreno',
         role: 'Analista de Datos',
-        image: 'assets/img/moises.jpg'
+        image: 'assets/img/moises.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Elias Campos Justiniano',
         role: 'Desarrollador Fullstack',
-        image: 'assets/images/maria.jpg'
+        image: 'assets/images/maria.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'María Esther Vargas',
         role: 'Desarollo avanzado en python',
-        image: 'assets/images/maria.jpg'
+        image: 'assets/images/maria.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     },
     {
         name: 'Eduardo Crespo',
         role: 'Edicion AudioVisual',
-        image: 'assets/images/eduardo.jpg'
+        image: 'assets/images/eduardo.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     }
     ,
     {
         name: 'Jesica Salvatierra',
         role: 'Edicion AudioVisual',
-        image: 'assets/img/jessi.jpg'
+        image: 'assets/img/jessi.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     }
     ,
     {
         name: 'Brisa Ortiz',
         role: 'Especialista en Contenido',
-        image: 'assets/img/brisa.jpg'
+        image: 'assets/img/brisa.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     }
     ,
     {
         name: 'Milan Arandia',
         role: 'Edicion AudioVisual',
-        image: 'assets/img/milan.jpg'
+        image: 'assets/img/milan.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     }
     ,
     {
         name: 'Yiseel Capiona',
         role: 'Especialista en Contenido',
-        image: 'assets/img/yiseel.jpg'
+        image: 'assets/img/yiseel.jpg',
+        facebook: '',
+        linkedin:'',
+        github:''
     }
     
 ];
@@ -88,7 +128,14 @@ valores = [
     { name: "Pasión", icon: "fa fa-heart" },
     { name: "Excelencia", icon: "fa fa-star" }
   ];
-  
+  team= [
+    { name: "Juan Pérez", role: "Full Stack Developer", img: "https://i.pravatar.cc/150?img=1" },
+    { name: "Ana Gómez", role: "Frontend Developer", img: "https://i.pravatar.cc/150?img=2" },
+    { name: "Carlos Rojas", role: "Backend Developer", img: "https://i.pravatar.cc/150?img=3" },
+    { name: "Lucía Méndez", role: "UI/UX Designer", img: "https://i.pravatar.cc/150?img=4" },
+    { name: "Pedro Sánchez", role: "DevOps Engineer", img: "https://i.pravatar.cc/150?img=5" },
+    { name: "María López", role: "Data Scientist", img: "https://i.pravatar.cc/150?img=6" }
+  ];
 
   constructor() { }
 
